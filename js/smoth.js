2fbfe17a075ccbc90f5b0db5f3e748c536a6c026
josbert2 <hernandezjosbert@gmail.com>
2018-06-11 14:32:05 -0430

    Fixed responsive

--- js/smoth.js
+++ js/smoth.js
@@ -1,7 +1,27 @@
 $('.navbar-nav-menu a').on('click', function() {
 
     var scrollAnchor = $(this).attr('data-scroll'),
-        scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+        scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 48;
+    console.dir(scrollPoint);
+
+    $('body,html').animate({
+        scrollTop: scrollPoint
+    }, 500);
+
+    return false;
+
+});
+$('.mouse-whell').on('click', function() {
+    var v = "data-anchor='who'";
+    $('body,html').animate({
+        scrollTop: 700
+    }, 500);
+
+})
+$('.nav-footer-f a').on('click', function() {
+
+    var scrollAnchor = $(this).attr('data-scroll-f'),
+        scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 48;
     console.dir(scrollPoint);
 
     $('body,html').animate({
@@ -18,7 +38,7 @@ $(window).scroll(function() {
     if (windscroll >= 100) {
 
         $('main section').each(function(i) {
-            if ($(this).position().top <= windscroll - 20) {
+            if ($(this).position().top <= windscroll - 150) {
                 $('.navbar-nav-menu a.active').removeClass('active');
                 $('.navbar-nav-menu a').eq(i).addClass('active');
             }
